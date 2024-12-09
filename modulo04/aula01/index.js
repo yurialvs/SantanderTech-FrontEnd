9f0c082110033a799aa7b299de47f4a06fb55dab
Yuri <lops.yuri@gmail.com>
2024-12-09 14:04:31 -0300

    Updated

---
+++ modulo04/aula01/index.js
@@ -0,0 +1,17 @@
+console.log("Hello, world");
+
+// Variáveis
+
+//JavaScript possui tipagem dinâmica: infere os tipos de dados
+
+var nomeDoAluno = 'Yuri'; // Camel case como padrão
+var idade = 27; // number
+var altura = 1.77; // number
+var estudando = true; // boolean
+
+console.log(nomeDoAluno);
+console.log(idade);
+console.log(altura);
+console.log(estudando);
+
+
