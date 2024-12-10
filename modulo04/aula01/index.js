89069241d1dc7506ad0f0e8ea362607d57be760a
Yuri <lops.yuri@gmail.com>
2024-12-10 08:43:39 -0300

    Updated

--- modulo04/aula01/index.js
+++ modulo04/aula01/index.js
@@ -3,15 +3,41 @@ console.log("Hello, world");
 // Variáveis
 
 //JavaScript possui tipagem dinâmica: infere os tipos de dados
-
+// var(variável) = (string)
 var nomeDoAluno = 'Yuri'; // Camel case como padrão
 var idade = 27; // number
 var altura = 1.77; // number
 var estudando = true; // boolean
 
+// Retorna as strings atribuidas as variáveis
 console.log(nomeDoAluno);
 console.log(idade);
 console.log(altura);
 console.log(estudando);
 
+// Retorna o tipo variável
+console.log(typeof estudando);
 
+console.log(typeof altura, altura);
+
+// Declarando uma  variável, sem atribuir uma string
+var semConteudo;
+console.log(semConteudo);
+
+// Declarando variáveis na mesma linha
+var curso = 'Front-end em React', topico = 'JavaScript Básico I';
+console.log(curso, topico);
+
+// Tipos de variáveis
+// Não utilize var nos códigos do JS
+
+// Permite alterar dados
+let nota = 10;
+
+// Não permite alterar
+const mediaDoAluno = 8;
+
+nota = 9;
+// mediaDoAluno = 5;  -> Não é permitido esta ação
+console.log(nota);
+console.log(mediaDoAluno);
