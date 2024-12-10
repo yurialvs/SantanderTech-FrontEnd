b806c51fb79a6b774feaad68f89d7f0e30086bfe
Yuri <lops.yuri@gmail.com>
2024-12-10 10:28:12 -0300

    Updated

---
+++ modulo04/aula01/browser.js
@@ -0,0 +1,3 @@
+const idade = window.prompt('Qual é a sua idade?');
+
+console.log('O usuário tem', idade, 'anos de idade.')
---
+++ modulo04/aula01/index02.js
@@ -0,0 +1,12 @@
+const nota1 = 10;
+const nota2 = 8;
+
+const somaDasNotas = nota1 + nota2;
+console.log('Soma das notas: ', somaDasNotas);
+
+let mediaDasNotas = somaDasNotas / 2;
+console.log('Média das notas: ', mediaDasNotas);
+
+const numero = 16;
+const raizDoNumero = Math.sqrt(numero);
+console.log('Raiz quadrada de 16: ', raizDoNumero)
