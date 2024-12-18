f81e09c3de027491b3a2c789544bfd37ed5ef1d2
Yuri <lops.yuri@gmail.com>
2024-12-18 12:06:52 -0300

    Updated

--- modulo05/aula07/index.ts
+++ modulo05/aula07/index.ts
@@ -8,8 +8,19 @@ class Pessoa {
     idade: number;
     altura: number;
 
+    // métodos: ação (função)
 
-    // métodos
+    // método construtor
+    constructor(nome: string, idade: number, altura: number) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
 
-
+    dormir() {
+        console.log('Dormindo...');
+    }
 }
+
+// Criando uma pessoa (individuo/objeto) a partir da definição da classe Pessoa
+const pessoa1 = new Pessoa('Yuri', 28, 1.81);
