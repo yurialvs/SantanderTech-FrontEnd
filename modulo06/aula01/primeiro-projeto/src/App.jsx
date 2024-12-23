bcc86d5c035f89748a9c560c8c615a7786449a86
Yuri <lops.yuri@gmail.com>
2024-12-23 08:33:33 -0300

    Updated

---
+++ modulo06/aula01/primeiro-projeto/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+// Componente em classe é uma classe que herda a classe Component do React,
+// e retorna HTML dentro do método render.
+
+class App extends React.Component {
+
+  // Método responsável por renderizar o conteúdo HTML do nosso componente
+  render() {
+    return (
+      <header>
+        <nav>
+          <div className='nav-brand'>
+            <img src="" alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+
+          <ul className='nav-list'>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            
+            <li>
+              <a href="/">Trending</a>
+            </li>
+
+            <li>
+              <a href="/">Categories</a>
+            </li>
+
+            <li>
+              <a href="/">About us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default App;
